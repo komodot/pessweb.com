9bffa5e05c8504f9bbbae0e29e4410deafbfe553
Enrique Garcia <enrique@inodata.com.mx>
2012-08-07 18:36:00 -0500

    Twig funcionando con un pequeño ejemplo en el controllador principal.

--- web/index.php
+++ web/index.php
@@ -3,8 +3,14 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path' => __DIR__.'/../views',
+));
+
 $app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
+    return $app['twig']->render('hello.twig', array(
+      'name' => $name,
+    ));
 });
 
 $app->run();
