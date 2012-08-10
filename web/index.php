8733d5ece71b79e68ddec7fcc9e41d968fdb8333
Enrique Garcia <enrique@inodata.com.mx>
2012-08-09 19:38:11 -0500

    UrlGeneratorServiceProvider agregado, TODO: generar el menu de navegacion.

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__.'/../views',
 ));
