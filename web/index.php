93877a67e4af9a710f177d72bc18ef0bfc9c3ded
Enrique Garcia <enrique@inodata.com.mx>
2012-08-15 17:56:40 -0500

    Configuracion de menu basica. .htaccess agregado aplicando mod_rewrite

--- web/index.php
+++ web/index.php
@@ -9,17 +9,19 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__.'/../views',
 ));
 
-$app->get('/hello/{name}', function ($name) use ($app) {
+/*$app->get('/hello/{name}', function ($name) use ($app) {
     return $app['twig']->render('hello.twig', array(
       'name' => $name,
     ));
 });
+*/
 
 $main = array('test');
 
 $app->get('/news', function () use ($app) {
   return $app['twig']->render('news.twig');
-});
+})
+->bind('news');
 
 $app->get('/navmenu/{section}', function ($section = 'home') use ($app) {
   return $app['twig']->render('navmenu.twig', array('section' => $section));
@@ -29,6 +31,7 @@ $app->get('/', function () use ($app, $main) {
   return $app['twig']->render('main.twig', array(
     'content' => $main,
   ));
-});
+})
+->bind('home');
 
 $app->run();
